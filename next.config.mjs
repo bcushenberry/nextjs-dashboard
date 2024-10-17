51e90a068a39b3981e8e69f8ea5bb6bbd7d45d58
bcushenberry <cushenberry@gmail.com>
2024-10-17 11:16:15 +0900

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,13 @@ const nextConfig = {
   experimental: {
     ppr: 'incremental',
   },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
 };
- 
-export default nextConfig;
 
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+export default nextConfig;
